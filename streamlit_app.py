e8d8f17f94d983a41c5ede25c15325e631a44dc7
senabj <senabj3808@gmail.com>
2025-05-27 07:59:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,11 @@ st.title("Selamat Datang di Website SenaBayhaqiJammaz")
 st.write(
     "ngodingseru bersama senabayhaqijammaz."
 )
+st.title("Aplikasi Sederhana")
+st.header("Aplikasi Mengecek Genap/Ganjil")
+angka = st.number_input("Tulis Sebuah Angka:", value=0, step=1)
+
+if (angka % 2 ) == 0:
+    st.write(f"{angka} adalah Bilangan Genap")
+else:
+    st.write(f"{angka} adalah Bilangan Ganjil")
